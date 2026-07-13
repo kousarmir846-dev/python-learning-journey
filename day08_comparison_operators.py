cb97f99f5ee028ca0ec7d9ebabfccebf57db303a
Kousar Mir <kousarmir846@gmail.com>
2026-07-13 20:07:50 +0500

    Added Day 9 - If Else Statements

---
+++ day08_comparison_operators.py
@@ -0,0 +1,15 @@
+print("========== camparison operators ==========")
+a = 10 
+b = 5 
+print("a =", a)
+print("b =", b)
+print("Equal:", a == b)
+print("Not Equal:", a != b)
+print("Greater Than:", a > b)
+print("Less Than:", a < b)
+print("Greater or Equal:", a >= b)
+print("Less or Equal:", a <= b)
+points = 35
+points = 45
+print(points)
+
---
+++ day09_if_statements.py
@@ -0,0 +1,24 @@
+age = 17 
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are not an adult.")
+age = 18 
+if age >= 18:
+    print("You are an adult.")
+    marks = 85
+    if marks >=90:
+        print("You have an A grade.")
+    elif marks >= 80:
+        print("You have a B grade.")
+    else:
+        print("You have a C grade.")
+age = 16
+if age >= 18:
+    print("you can vote.")
+    number = 10
+    if number == 10:
+        print("correct")
+    else:
+        print("wrong")
+        
